393231153693303915811de9c852a14d4e0bcdb1
manuel-sperrle-stb-de <manuel.sperrle@stb.de>
2023-09-20 23:27:26 +0200

    updated: ps module / traefik tls

--- _assets/ps1/Modules/OnBoardModule/0.1/Public/profile.ps1
+++ _assets/ps1/Modules/OnBoardModule/0.1/Public/profile.ps1
@@ -3,12 +3,10 @@
 'Get/Set PSRepository PSGallery Trusted'
 if (-not ((Get-PSRepository PSGallery).InstallationPolicy -eq 'Trusted')) { Set-PSRepository PSGallery -InstallationPolicy Trusted }
 
+'Adding OnBoardModule to PSModulePath'
+$ModulePath =  Get-Item (Join-Path $PSScriptRoot '../../..')
+if ($Env:PSModulePath -notmatch $ModulePath) { $Env:PSModulePath += (':{0}' -f $ModulePath.FullName) }
+
 'Install/Import Module DockerCompletion'
 if (-not (Get-InstalledModule DockerCompletion -ErrorAction SilentlyContinue)) { Install-Module DockerCompletion }
 if (-not (Get-Module DockerCompletion)) { Import-Module DockerCompletion }
-
-'Dot-sourcing *.Function.ps1 from $PSScriptRoot'
-Get-ChildItem $PSScriptRoot -Recurse -Filter '*.Function.ps1' | ForEach-Object { 
-    $_.FullName
-    . $_.FullName 
-}
